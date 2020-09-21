949afe72d1353d56210f724e27960dcd4a3830e9
09ghostrider <57857850+007-NIKSH@users.noreply.github.com>
2020-09-21 14:55:08 +0530

    MADE A FEW CHANGES

--- TIC TAC TOE.py
+++ TIC TAC TOE.py
@@ -3,7 +3,7 @@ from tkinter import *
 root = Tk()
 root.title("TIC TAC TOE")
 
-text = Entry(root, font = ("calibri", 16))
+text = Entry(root, font = ("calibri", 12))
 text.pack(fill = X, padx = 5, pady = 5, ipadx = 5, ipady = 5)
 
 board = ["-", "-", "-",
@@ -116,37 +116,37 @@ frame.pack(side = TOP, anchor = NW)
 frame1 = Frame(frame)
 frame1.pack()
 
-button_1 = Button(frame1, text = "", width = 9, height = 3, command = lambda:Add_Text(0, player))
+button_1 = Button(frame1, text = "", width = 7, height = 3, command = lambda:Add_Text(0, player))
 button_1.pack(side = LEFT)
 
-button_2 = Button(frame1, text = "", width = 9, height = 3, command = lambda:Add_Text(1, player))
+button_2 = Button(frame1, text = "", width = 7, height = 3, command = lambda:Add_Text(1, player))
 button_2.pack(side = LEFT)
 
-button_3 = Button(frame1, text = "", width = 9, height = 3, command = lambda:Add_Text(2, player))
+button_3 = Button(frame1, text = "", width = 7, height = 3, command = lambda:Add_Text(2, player))
 button_3.pack(side = LEFT)
 
 frame2 = Frame(frame)
 frame2.pack()
 
-button_4 = Button(frame2, text = "", width = 9, height = 3, command = lambda:Add_Text(3, player))
+button_4 = Button(frame2, text = "", width = 7, height = 3, command = lambda:Add_Text(3, player))
 button_4.pack(side = LEFT)
 
-button_5 = Button(frame2, text = "", width = 9, height = 3, command = lambda:Add_Text(4, player))
+button_5 = Button(frame2, text = "", width = 7, height = 3, command = lambda:Add_Text(4, player))
 button_5.pack(side = LEFT)
 
-button_6 = Button(frame2, text = "", width = 9, height = 3, command = lambda:Add_Text(5, player))
+button_6 = Button(frame2, text = "", width = 7, height = 3, command = lambda:Add_Text(5, player))
 button_6.pack(side = LEFT)
 
 frame3 = Frame(frame)
 frame3.pack()
 
-button_7 = Button(frame3, text = "", width = 9, height = 3, command = lambda:Add_Text(6, player))
+button_7 = Button(frame3, text = "", width = 7, height = 3, command = lambda:Add_Text(6, player))
 button_7.pack(side = LEFT)
 
-button_8 = Button(frame3, text = "", width = 9, height = 3, command = lambda:Add_Text(7, player))
+button_8 = Button(frame3, text = "", width = 7, height = 3, command = lambda:Add_Text(7, player))
 button_8.pack(side = LEFT)
 
-button_9 = Button(frame3, text = "", width = 9, height = 3, command = lambda:Add_Text(8, player))
+button_9 = Button(frame3, text = "", width = 7, height = 3, command = lambda:Add_Text(8, player))
 button_9.pack(side = LEFT)
 
 root.mainloop()
